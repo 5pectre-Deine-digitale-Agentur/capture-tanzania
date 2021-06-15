b206bf681697a56bfd9006c0161e258d004061ca
derEinzigWahreSchnabel <christian.schnabel@5pectre.com>
2021-06-15 21:55:03 +0200

    initial upload

---
+++ page.php
@@ -0,0 +1,17 @@
+<?php
+/*		STANDARDSEITENTEMPLATE
+
+	*		TODO: Standardseitentemplate erstellen.
+	* 	TODO: ACF einbinden
+	*		TODO: AOS-Animationen erstellen
+
+*/
+get_header(); ?>
+
+<?php
+ <h1>Hallo Welt</h1>
+ <h2>Was ist hier los??</h2>
+include 'sections/content.php';
+?>
+
+<?php get_footer(); ?>
